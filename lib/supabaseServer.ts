631e54670c7a7265c6713e0b8ce21fd34af37d63
SEOsakht <56796219+SEOsakht@users.noreply.github.com>
2025-09-18 22:52:59 +0000

    Update supabaseServer.ts 00011

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -5,6 +5,6 @@ export function createSupabaseServerClient() {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies }
+    { cookies() }
   )
 }
